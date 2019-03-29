09a5e6f558e0378a14ac14f63fdc2d6d73031d22
vikas0526 <49014529+vikas0526@users.noreply.github.com>
2019-03-29 09:32:00 -0400

    Delete ItemBidShouldBeHigherException.java

--- ItemBidShouldBeHigherException.java
+++
@@ -1,6 +0,0 @@
-public class ItemBidShouldBeHigherException extends Exception{
-
-        public ItemBidShouldBeHigherException() {
-            super("This is a ItemBidShouldBeHigherException, Bid should be higher than previous bid");
-        }
-}
